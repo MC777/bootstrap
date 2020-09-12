2339ea183182da52fbea724d2a59670ca574ca77
Marek Ciesla <ciesla.marek@gmail.com>
2020-09-12 23:56:22 +0200

    feat: add locationService

--- src/main/java/com/udacity/bootstrap/service/LocationService.java
+++ src/main/java/com/udacity/bootstrap/service/LocationService.java
@@ -6,4 +6,7 @@ import java.util.List;
 
 public interface LocationService {
     List<Location> retrieveLocations();
+    List<String> retrieveLocationName();
+    String retrieveLocationById(Long id);
+    List<String> retrieveLocationAddress();
 }
