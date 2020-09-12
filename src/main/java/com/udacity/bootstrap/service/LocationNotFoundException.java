5fb7d827f831c3d5636b7e1ba41d04afd73918c0
Marek Ciesla <ciesla.marek@gmail.com>
2020-09-12 23:58:00 +0200

    feat: add Location Not Foud Exception

--- src/main/java/com/udacity/bootstrap/service/LocationNotFoundException.java
+++ src/main/java/com/udacity/bootstrap/service/LocationNotFoundException.java
@@ -1,2 +1,14 @@
-package com.udacity.bootstrap.service;public class LocationNotFoundException {
+package com.udacity.bootstrap.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Location not found")
+public class LocationNotFoundException extends RuntimeException {
+    public LocationNotFoundException() {
+    }
+    public LocationNotFoundException(String message) {
+        super(message);
+    }
 }
+
