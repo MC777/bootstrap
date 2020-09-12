b19074d066d942becf2f37b31246d08912290cbe
Marek Ciesla <ciesla.marek@gmail.com>
2020-09-12 23:56:39 +0200

    feat: add locationServiceImpl

--- src/main/java/com/udacity/bootstrap/service/LocationServiceImpl.java
+++ src/main/java/com/udacity/bootstrap/service/LocationServiceImpl.java
@@ -6,13 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-    @Service
+@Service
     public class LocationServiceImpl implements LocationService {
         @Autowired
         LocationRepository locationRepository;
 
         public List<Location> retrieveLocations() {
             return (List<Location>) locationRepository.findAll();
+        }
+
+        @Override
+        public List<String> retrieveLocationName() {
+            return (List<String>) locationRepository.findAllNames();
+        }
+
+        @Override
+        public String retrieveLocationById(Long id) {
+            Optional<String> optionalBreed = Optional.ofNullable(locationRepository.findLocationById(id));
+            String breed = optionalBreed.orElseThrow(LocationNotFoundException::new);
+            return breed;
+        }
+
+        @Override
+        public List<String> retrieveLocationAddress() {
+            return (List<String>) locationRepository.findAllLocations();
+
         }
     }
